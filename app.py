8d76f190546d4f208071c285bd589af620f90295
Davej66 <daijacobs@gmail.com>
2021-12-23 11:09:11 +0000

    Allow user to submot to db

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_reviews")
 def get_reviews():
-    tasks = list(mongo.db.tasks.find())
-    return render_template("reviews.html", tasks=tasks)
+    reviews = list(mongo.db.reviews.find())
+    return render_template("reviews.html", reviews=reviews)
 
 
 
@@ -100,8 +100,21 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_review")
+@app.route("/add_review", methods=["GET", "POST"])
 def add_review():
+    if request.method == "POST":
+        review = {
+            "genre_name": request.form.get("genre_name"),
+            "review_name": request.form.get("review_name"),
+            "review_description": request.form.get("review_description"),
+            "review_rating": request.form.get("review_rating"),
+            "book_cover": request.form.get("book_cover"),
+            "created_by": session["user"]
+        }
+        mongo.db.reviews.insert_one(review)
+        flash("Review Successfully Added")
+        return redirect(url_for("get_reviews"))
+        
     genres = mongo.db.genre.find().sort("genre_name", 1)
     return render_template("add_review.html", genres=genres)
 
